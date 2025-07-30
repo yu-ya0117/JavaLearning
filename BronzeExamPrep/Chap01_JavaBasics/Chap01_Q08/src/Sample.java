794a5379fa54b5426a055e31278986da6ba82f70
yu-ya0117 <talesoftoho@gmail.com>
2025-07-30 14:43:03 +0900

    初期コミット：第1章8問目のサンプルプログラムを追加

---
+++ BronzeExamPrep/Chap01_JavaBasics/Chap01_Q08/src/Sample.java
@@ -0,0 +1,11 @@
+public class Sample {
+    public static void main (String[] args){
+        Test test = new Test();
+        test.hello();
+    }
+}
+class Test{
+    public void hello(){
+        System.out.println("Hello, World!");
+    }
+}
