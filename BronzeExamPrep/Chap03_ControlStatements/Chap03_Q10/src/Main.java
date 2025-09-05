7c8888aac8ae6615f3d416b01c8310ec7f5b552f
yu-ya0117 <talesoftoho@gmail.com>
2025-09-05 14:50:29 +0900

    11回目のコミット：前置インクリメントと後置インクリメントのサンプルプログラムを追加

---
+++ BronzeExamPrep/Chap03_ControlStatements/Chap03_Q10/src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(++a);    //++aはaが1加算された状態で出力
+        System.out.println(b);
+        System.out.println(b++);    //b++はbが加算される前に出力
+    }
+}
