b55c2dbc0135e23d1aad6df62fc21579db4ec359
yu-ya <talesoftoho@gmail.com>
2025-10-10 14:21:42 +0900

    Update HelloController for PathVariable and clean up Spring Boot project files

--- StudyProjects/HelloSpringBoot/src/main/java/com/example/hello_springboot/HelloController.java
+++ StudyProjects/HelloSpringBoot/src/main/java/com/example/hello_springboot/HelloController.java
@@ -1,13 +1,35 @@
 package com.example.hello_springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+//JSON形式として返すためのクラス
+class Greeting {
+    private String message;
+    private String language;
+
+    public Greeting(String message,String language) {
+        this.message = message;
+        this.language = language;
+    }
+
+    //以下出力用getter
+    public String getMessage() {
+        return message;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+}
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, Spring Boot!";
+    //「/hello/(自分の名前)」の形でアクセスできるようにする
+    @GetMapping("/hello/{name}")
+    public Greeting hello(@PathVariable String name) {
+        return new Greeting("Hello," + name + "!", "English");
     }
 }
